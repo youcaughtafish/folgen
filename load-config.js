9ff973124907c0ba909cbd844c0168304090d069
Dylan Moore <dzmoore@gmail.com>
2015-07-28 20:19:03 -0400

    reading files working

--- load-config.js
+++ load-config.js
@@ -1,18 +1,85 @@
-var assert = require('assert');
+var fs = require('fs');
+var through2 = require('through2');
 
 var loadConfig = function(db, callback) {
+  var config = {};
+  findInputFilenames(db, config, function(err, updatedConfig) {
+    if (err) throw err;
+
+    callback(err, updatedConfig); 
+  });
+}
+
+var readInputFile = function (filename) {
+  var lineBuffer = '';
+  var lineNumber = 0;
+  var rs = fs.createReadStream(filename, {
+    encoding: 'utf-8'
+  });
+
+  var pushLine = function(dst, filename, line) {
+    dst.push({
+      filename:       filename,
+      fileLineText:   line,
+      fileLineNumber: ++lineNumber
+    });
+  };
+
+  var h = 0;
+  return rs.pipe(through2.obj(function (chunk, enc, callback) {
+    lineBuffer += chunk;
+
+    if (chunk.indexOf('\n')) {
+      var lines = lineBuffer.split('\n');
+
+      var bufferEndsInNl = lineBuffer[lineBuffer.length-1] === '\n';
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (i === lines.length-1) {
+          if (bufferEndsInNl) {
+            pushLine(this, filename, line);
+            lineBuffer = '';
+          } 
+
+        } else {
+          pushLine(this, filename, line);
+        }
+      }
+
+      if (bufferEndsInNl) {
+        lineBuffer = '';
+      
+      } else {
+        lineBuffer = lines[lines.length-1];
+      }
+
+    } 
+
+    callback();
+
+  }, function(callback) { // flush function
+    if (lineBuffer) {
+      pushLine(this, filename, lineBuffer);
+    }
+
+    callback();
+  }));
+}
+
+var findInputFilenames = function(db, config, callback) {
   var collection = db.collection('config');
 
   collection.find({
     inputFilenames: { $exists: true }
   }).toArray(function(err, items) {
-    if (!err) {
-      callback(err, null);
+      config.inputFilenames = items;
 
-    } else {
-      callback(null, items);
-    }
+      callback(
+        err ? err : null, 
+        err ? null : config
+      );
   }); 
 }
 
 module.exports.loadConfig = loadConfig;
+module.exports.readInputFile = readInputFile;
--- test/load-config.js
+++ test/load-config.js
@@ -1,17 +1,27 @@
 var mongo = require('mongodb').MongoClient;
 var should = require("should");
+var fs = require('fs');
 var loadConfig = require("../load-config.js").loadConfig;
+var readInputFile = require("../load-config.js").readInputFile;
+
 var url   = 'mongodb://localhost:27017/tests';
+
+var inputFiles = {
+  'test1.log': "one\ntwo\n\nfour\nfive",
+  'test2.log': "[12:12:12T02/02/02] [INFO] - blah 127.0.0.1 blah blah <xml>\n" +
+               "[12:12:13T02/02/02] [DEBUG] - <xml>",
+  'test3.log': "[12:12:12T02/02/02] [INFO] - blah 127.0.0.1 blah blah <xml>\n" +
+               "[12:12:13T02/02/02] [DEBUG] - <xml>\n"+
+               "[12:12:14T02/02/02] [INFO] - blah 127.0.0.1 blah blah <xml>\n" +
+               "[12:12:15T02/02/02] [DEBUG] - <xml>\n"
+};
+
 
 mongo.connect(url, function(err, db) {
   db.config.insert({
     inputFilenames: { $exists: true }
   }, {
-    inputFilenames: [
-      'test1.out',
-      'test2.out',
-      'test3.out'
-    ]
+    inputFilenames: Object.keys(inputFiles)
   }, {
     upsert: true
   });
@@ -19,19 +29,39 @@ mongo.connect(url, function(err, db) {
   db.close();
 });
 
+for (var eaFilename in inputFiles) {
+  fs.writeFile("./test/"+eaFilename, inputFiles[eaFilename], function(err) {
+    if (err) throw err;
+  });
+}
+
 describe('load-config', function() {
   describe('loadConfig', function () {
-     
     it('should return a list of filenames', function () {
       mongo.connect(url, function(err, db) {
-        loadConfig(db, function(err, results) {
-          results.should.exist.
+        loadConfig(db, function(err, config) {
+          config.inputFilenames.should.exist.
             and.be.an.instanceOf(Array).
             and.not.have.lengthOf(0);
         });
 
         db.close();
       });
+    });
+  });
+
+  describe('readInputFile', function () {
+    it('should read the input file', function(done) {
+      var lines = [];
+      readInputFile('test/'+Object.keys(inputFiles)[0]).
+        on('data', function(obj) {
+          lines.push(obj);
+        }).
+        on('end', function() {
+          lines.should.have.lengthOf(5);
+
+          done();
+        });
     });
   });
 });
